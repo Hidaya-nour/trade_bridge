38e6760e8e3a1ac9f86468e4b0f856729f2198b2
Hidaya <nourhidaya921@gmail.com>
2026-02-06 10:11:54 +0300

    minor restructure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,7 @@
 import React from "react";
-import GoogleLoginButton from "../components/GoogleLoginButton";
-
 const App: React.FC = () => {
-  
   return (
     <div className="App">
-      <h1>Login with Google </h1>
-      <GoogleLoginButton />
     </div>
   );
 };
